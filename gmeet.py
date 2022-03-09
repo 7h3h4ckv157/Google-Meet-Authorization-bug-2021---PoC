0316e4f0a29798554e43b12ae8e669a6294d9414
Kiran PP <58929890+7h3h4ckv157@users.noreply.github.com>
2022-03-09 08:32:16 +0530

    Add files via upload
    Imported the dependencies & make sure you've installed each.

---
+++ gmeet.py
@@ -0,0 +1,25 @@
+#!/usr/7h3h4ckv157/py
+
+import webbrowser
+import os
+import time
+import pyautogui
+
+os.system("toilet --filter metal '7h3h4ck157' | boxes -d cat -a hc -p h8 | lolcat")
+print("\t\t [[	GOOGLE-MEET AUTOMATION	]]")
+
+link = input("\n         \t----->\tProvide your input:")
+print("\n\n")
+
+os.system("toilet -f smblock 'Are  you  watching  closely...?'")
+
+
+
+time.sleep(3)
+
+for i in range(10):
+  webbrowser.open(link)
+  pyautogui.moveTo(843, 507, duration=5)
+  pyautogui.click()
+  pyautogui.moveTo(920, 457, duration=3)
+  pyautogui.click()
